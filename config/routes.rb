1b3d0e244236b0ee771d98a18d3276c7e40c78a7
thadeus nyariki <thadeusnyariki94@gmail.com>
2024-09-14 18:42:23 +0300

    webhook

--- config/routes.rb
+++ config/routes.rb
@@ -35,5 +35,6 @@ Rails.application.routes.draw do
   post "checkout" => "checkouts#create"
   get "success" => "checkouts#success"
   get "cancel" => "checkouts#cancel"
+  post "webhooks" => "webhooks#stripe"
   
 end
